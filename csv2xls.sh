9b1f73de44ba8a633ca263f79f878e0e03b1a1ef
Razvan Deaconescu <razvan.deaconescu@cs.pub.ro>
2017-04-09 20:29:31 +0300

    Quote variables in shell scripts

--- csv2xls.sh
+++ csv2xls.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] || [ ! -d $1 ]; then
+if [ $# -ne 1 ] || [ ! -d "$1" ]; then
 	echo "Usage: $0 DIR_DATA"
 	exit 1
 fi
 
-pushd $1 &> /dev/null
+pushd "$1" &> /dev/null
 for file in $(ls *-prelucrat.csv); do
 	xls_file=$(basename $file .csv).xls
 	echo "Convert $file to $xls_file."
-	ssconvert $file $xls_file &> /dev/null
+	ssconvert "$file" "$xls_file" &> /dev/null
 done
 popd &> /dev/null
